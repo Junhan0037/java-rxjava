2c85c36ab4e933940513f597614d44f26ace94e4
Junhan <wnsgks0037@naver.com>
2021-01-04 15:47:07 +0900

    ADD : Observable & Flowable

---
+++ src/main/java/com/javarxjava/utils/DateUtil.java
@@ -0,0 +1,13 @@
+package com.javarxjava.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DateUtil {
+
+    public static String getNowDate(){
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                .format(Calendar.getInstance().getTime());
+    }
+
+}
---
+++ src/main/java/com/javarxjava/utils/NumberUtil.java
@@ -0,0 +1,9 @@
+package com.javarxjava.utils;
+
+public class NumberUtil {
+
+    public static int randomRange(int min, int max){
+        return (int)(Math.random() * (max - min + 1)) + min;
+    }
+
+}
